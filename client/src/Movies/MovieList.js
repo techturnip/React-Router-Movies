6ec6e6da22f36d5d21cff862c4e320acee9e96fc
Tyler Turnipseed <tyler@techturnip.us>
2019-06-22 12:41:52 -0400

    replaced repeated JSX in Movie.js with MovieCard component and removed the MovieDetails function from MovieList

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -34,25 +34,3 @@ export default class MovieList extends Component {
     );
   }
 }
-
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <Link to={`/movies/${movie.id}`} className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </Link>
-  );
-}
